3761c9424b1b6738265c737f4e642d612727d16d
Bushra Ali <152624099+Bushraali12@users.noreply.github.com>
2024-07-03 20:53:47 +0500

    Add files via upload

---
+++ Mobile/Scripts/Utilities/MobileDisableAutoSwitchControls.cs
@@ -0,0 +1,27 @@
+
+using UnityEngine;
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+using UnityEngine.InputSystem;
+#endif
+
+public class MobileDisableAutoSwitchControls : MonoBehaviour
+{
+    
+#if ENABLE_INPUT_SYSTEM && (UNITY_IOS || UNITY_ANDROID) && STARTER_ASSETS_PACKAGES_CHECKED
+
+    [Header("Target")]
+    public PlayerInput playerInput;
+
+    void Start()
+    {
+        DisableAutoSwitchControls();
+    }
+
+    void DisableAutoSwitchControls()
+    {
+        playerInput.neverAutoSwitchControlSchemes = true;
+    }
+
+#endif
+    
+}
